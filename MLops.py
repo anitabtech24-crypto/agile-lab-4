58c1edf6d78d68909e3270214b036fb43ae017bb
anitabtech24-crypto <anitabtech24@rvu.edu.in>
2026-02-19 12:02:44 +0530

    Update MLops.py

--- MLops.py
+++ MLops.py
@@ -4,8 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
-X = [[30],[40],[50],[60],[20],[10],[70]]
-y = [0,1,1,1,0,0,1]
+X = [[30],[39],[40],[50],[60],[20],[10],[70]]
+y = [0,0,1,1,1,0,0,1]
 from sklearn.neighbors import KNeighborsClassifier
 classifier= KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2 )
 classifier.fit(X,y)
